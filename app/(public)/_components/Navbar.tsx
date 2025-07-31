d92940d27a1e239959510222dc2f52b41871f098
fernandes-draw <fernandes_draw@hotmail.com>
2025-07-31 16:11:41 -0300

    Creating Landing Page

---
+++ app/(public)/_components/Navbar.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import Logo from "@/public/globe.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { UserDropdown } from "./userDropdown";
+
+const navigationItems = [
+  { name: "Home", href: "/" },
+  { name: "Courses", href: "/courses" },
+  { name: "Dashboard", href: "/dashboard" },
+];
+
+export default function Navbar() {
+  const { data: session, isPending } = authClient.useSession();
+  return (
+    <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-backgraound/60">
+      <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+        <Link href="/" className="flex items-center space-x-2 mr-4">
+          <Image
+            src={Logo}
+            alt="Logo"
+            className="size-9 bg-orange-500 rounded-full"
+          />
+          <span className="font-bold">MarshalLMS.</span>
+        </Link>
+        {/* desktop navigation */}
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center space-x-2">
+            {navigationItems.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <ThemeToggle />
+
+            {isPending ? null : session ? (
+              <UserDropdown
+                email={session.user.email}
+                image={session.user.image || ""}
+                name={session.user.name}
+              />
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className={buttonVariants({
+                    variant: "secondary",
+                  })}
+                >
+                  Login
+                </Link>
+                <Link href="/login" className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,27 +1,42 @@
-"use client";
-
 import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+
+interface featuresProps {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+const features: featuresProps[] = [
+  {
+    title: "Comprehensive Courses",
+    description:
+      "Access a wide range of carefully curated courses designed by industry experts.",
+    icon: "📚",
+  },
+  {
+    title: "Interactive Learning",
+    description:
+      "Engage with hands-on activities, quizzes, and real-time feedback to enhance your understanding.",
+    icon: "🎮",
+  },
+  {
+    title: "Progress Tracking",
+    description:
+      "Monitor your learning journey with detailed progress tracking and personalized dashboard.",
+    icon: "📈",
+  },
+  {
+    title: "Community Support",
+    description:
+      "Join a vibrant community of learners and get support from peers and instructors.",
+    icon: "🤝",
+  },
+];
 
 export default function Home() {
-  const { data: session } = authClient.useSession();
-  const router = useRouter();
-
-  async function signOut() {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push("/");
-          toast.success("Signed out successfully.");
-        },
-      },
-    });
-  }
-
   return (
     <>
       <section className="relative py-20">
@@ -56,6 +71,19 @@ export default function Home() {
             </Link>
           </div>
         </div>
+      </section>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-32">
+        {features.map((feature, index) => (
+          <Card key={index} className="hover:shadow transition-shadow">
+            <CardHeader>
+              <div className="text-4xl mb-4">{feature.icon}</div>
+              <CardTitle>{feature.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-muted-foreground">{feature.description}</p>
+            </CardContent>
+          </Card>
+        ))}
       </section>
     </>
   );
